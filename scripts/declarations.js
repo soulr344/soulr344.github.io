6ec331bf26450f1a498641b94ec39fe4de3e7b33
soulr344 <bloodthirstyoff@gmail.com>
2021-09-01 16:55:09 +0545

    scripts: oopsie.. forgot to add declarations F
    Signed-off-by: soulr344 <bloodthirstyoff@gmail.com>

---
+++ scripts/declarations.js
@@ -0,0 +1,19 @@
+(function(window){
+    'use strict';
+    const _loadJS = (src, callback) => {
+        let script = document.createElement('script');
+        script.setAttribute("src", src);
+        script.onload = callback;
+        document.head.appendChild(script);
+    }
+    if (typeof(window.loadJS) === 'undefined'){
+        window.loadJS = _loadJS;
+    }
+    window.vh = window.innerHeight / 100;
+    window.getCurrYPosition = function(){
+        return window.pageYOffset;
+    }
+    window.isElementIntersecting = function(element){
+        return (getCurrYPosition() >= element.offsetTop - element.offsetHeight / 2);
+    }
+})(window);
